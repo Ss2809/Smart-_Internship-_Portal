3c65a0d51978e5223de274ba018e624ae4c3f63f
Sushil Pawar <sushilpawar2321@gmail.com>
2026-02-04 01:08:15 +0530

    Simplify sender email format in sendMail.jsdone

--- Backend/config/sendMail.js
+++ Backend/config/sendMail.js
@@ -4,7 +4,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 
 const sendOTP = async (to, otp) => {
   return await resend.emails.send({
-    from: "Smart Internship <onboarding@resend.dev>",
+    from:  "onboarding@resend.dev",
     to: to,
     subject: "Your OTP",
     html: `<h2>Your OTP is ${otp}</h2><p>Valid for 5 minutes.</p>`,
