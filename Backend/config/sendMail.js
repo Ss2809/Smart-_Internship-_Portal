e8b12de24c7a753cb94dc20b0db08a8a564d2292
Sushil Pawar <sushilpawar2321@gmail.com>
2026-02-04 01:13:31 +0530

    d

--- Backend/config/sendMail.js
+++ Backend/config/sendMail.js
@@ -4,7 +4,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 
 const sendOTP = async (to, otp) => {
   return await resend.emails.send({
-    from:  "onboarding@resend.dev",
+    from: "Smart Internship <no-reply@smartinternshipportal.com>",
     to: to,
     subject: "Your OTP",
     html: `<h2>Your OTP is ${otp}</h2><p>Valid for 5 minutes.</p>`,
