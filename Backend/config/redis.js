1fbbc59b8958042d9361d10d27a02ae5f802e873
Sushil pawar <sushilpawar2321@gmail.com>
2026-02-20 00:54:19 +0530

    Update redis.js

--- Backend/config/redis.js
+++ Backend/config/redis.js
@@ -3,6 +3,7 @@ import { createClient } from "redis";
 const client = createClient({
   url: process.env.REDIS_URL
 });
+console.log(process.env.REDIS_URL);
 
 client.on("error", (err) => console.log("Redis Client Error", err));
 
